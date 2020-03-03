c2db0a1cf890b14114f52802fb72557a9bc22e4b
zrajani <zainrajani2207@gmail.com>
2020-03-03 11:31:56 -0500

    Information on Gray scale Image

--- Image_Types.m
+++ Image_Types.m
@@ -1,10 +1,18 @@
 % To illustrate the types of Images 
 
+% A image may be thought of data matrix (ref: basic command image.m file)
+
 % Initialize
 
 clear all;
 close all;
 clc;
+
+%% Gray-Scale Image (a.k.a Intensity Images)
+% Defination: It is a data matrix whose values represent shades of gray.
+% Gray scale image are either of class unit8 or unit16 having integer
+% values ranging from [0,255] or [0,65535] respectively. If any other class
+% then values remain the same. 
 
 %% Binary Images
 
