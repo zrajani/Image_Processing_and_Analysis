25afc7127158fdd510df0b773586e962a31b1b60
zrajani <zainrajani2207@gmail.com>
2020-03-17 12:04:54 -0400

    Basic Usage

---
+++ Basic Usage of MATLAB/Basic_Usage.m
@@ -0,0 +1,17 @@
+% M- Function Programming 
+
+% Initialise 
+
+clear all;
+close all;
+clc;
+
+
+% Function defination in MATLAB is function [outputs] = name(inputs)
+% For illustration purpose we shall be creating a function to compute image
+% stats 
+
+f=imread('office_1.jpg');
+
+
+
