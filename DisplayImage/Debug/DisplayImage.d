9d30430c17876e69523bde32d2445aaaaa65cf15
zrajani <zainrajani2207@gmail.com>
2020-04-07 22:05:28 -0400

    Basic OpenCV Image Display

---
+++ DisplayImage/Debug/DisplayImage.d
@@ -0,0 +1,1 @@
+DisplayImage.o: ../DisplayImage.cpp
---
+++ DisplayImage/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopencv_highgui -lopencv_imgcodecs -lopencv_ml -lopencv_core -lopencv_imgproc
+
