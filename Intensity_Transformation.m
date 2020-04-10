53ace3bdefd86360533f3ef3e6acb35139bcbcbf
zrajani <zainrajani2207@gmail.com>
2020-04-10 16:58:12 -0400

    PLaying with Image Intensity

--- Intensity_Transformation.m
+++ Intensity_Transformation.m
@@ -1,7 +1,17 @@
+
+% Initialize
 clear all;
 close all;
 clc;
 
+%% Reading Image
 f=imread('Jason_membrane.jpg');
 
+%% Complement of an Image
+
+f_comp=imcomplement(f);
+
+%% Intensity Transformation functions
+
 g1=imadjust(f,[0 1],[1 0]);
+g2=imadjust(f,[0.5 0.75],[1 0],2);
