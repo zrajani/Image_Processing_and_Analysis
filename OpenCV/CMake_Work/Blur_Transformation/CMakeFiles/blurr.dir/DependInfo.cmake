68f28f7841e92bd2327ed388bdaf632d7d135b18
zrajani <zainrajani2207@gmail.com>
2021-04-29 23:39:04 -0400

    CMake related files

---
+++ OpenCV/CMake_Work/Blur_Transformation/CMakeFiles/blurr.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zain/Desktop/GITHUB_FILES/Image_Beautification_and_Advancement/OpenCV/CMake_Work/Blur_Transformation/blurr.cpp" "/home/zain/Desktop/GITHUB_FILES/Image_Beautification_and_Advancement/OpenCV/CMake_Work/Blur_Transformation/CMakeFiles/blurr.dir/blurr.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpenCV/CMake_Work/Blur_Transformation/CMakeFiles/blurr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blurr.dir/blurr.cpp.o"
+  "blurr.pdb"
+  "blurr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blurr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
