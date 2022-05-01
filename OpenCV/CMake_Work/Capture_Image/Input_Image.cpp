f1552f8a6caffe03081b2120226c1a91ce3fda44
zrajani <zainrajani2207@gmail.com>
2022-05-01 17:20:15 -0400

    Added Comments to file for better understanding

--- OpenCV/CMake_Work/Capture_Image/Input_Image.cpp
+++ OpenCV/CMake_Work/Capture_Image/Input_Image.cpp
@@ -7,22 +7,24 @@ int main( int argc, char** argv){
   cv::VideoCapture cap;
 
   if(argc==1){
-    cap.open(0);
+    cap.open(0);    // Check if Camera is Open
   }else{
     cap.open(argv[1]);
   }
   if (!cap.isOpened()){
-    std::cerr << "Couldn't Open Capture Camera"<< std::endl;
+    std::cerr << "Couldn't Open Capture Camera"<< std::endl;  // Throw an error
     return -1;
   }
 
-  //cv::namedWindow("Output Window", cv::WINDOW_AUTOSIZE);
+  //cv::namedWindow("Output Window", cv::WINDOW_AUTOSIZE); <---- Ignore as the line will open unnecessary window
+
+  // Start to operate in Video Mode
   cv::Mat frame;
   for(;;){
     cap>>frame;
     if (frame.empty()) break;
-    cv::imshow("Capturing Image",frame);
-    if ((char)cv::waitKey(33)>=0){imwrite("Zain.jpg",frame);break;}
+    cv::imshow("Capturing Image",frame);  // Display the window
+    if ((char)cv::waitKey(33)>=0){imwrite("Zain.jpg",frame);break;}  // When closing the last window capture the file and save it
   }
 
   return 0;
