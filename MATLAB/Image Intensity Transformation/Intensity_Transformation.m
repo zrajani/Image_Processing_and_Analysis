4581b73145d95b7799de52f9e6e9e6e82df86267
zrajani <zainrajani2207@gmail.com>
2022-08-28 15:39:50 -0400

    Minor Chnages to coide and added some comments

--- MATLAB/Image Intensity Transformation/Intensity_Transformation.m
+++ MATLAB/Image Intensity Transformation/Intensity_Transformation.m
@@ -8,7 +8,7 @@ clear all;
 close all;
 clc;
 
-imshow('Lena.png');
+%imshow('Lena.png');
 
 %% Reading Image
 f=imread('Jason_membrane.jpg');  % Can be replaced by any image by your choice (any type of Image)
@@ -20,7 +20,7 @@ f_comp=imcomplement(f);
 
 %% Intensity Transformation functions
 
-g1=imadjust(f,[0 1],[1 0]);
+g1=imadjust(f,[0 1],[1 0]); % Getting Negative Image Alternative suggestion (Negative Image)
 g2=imadjust(f,[0.5 0.75],[1 0],2);
 
 %% Display Images 
@@ -29,9 +29,13 @@ g2=imadjust(f,[0.5 0.75],[1 0],2);
 figure;
 subplot (2,2,1)
 imshow(f);
+title('Input Image')
 subplot (2,2,2)
 imshow(g1)
+title('Negative Image')
 subplot(2,2,3)
 imshow(f_comp)
+title('Negative Image')
 subplot(2,2,4)
 imshow(g2)
+title('Adjusting Intensities with parameters')
