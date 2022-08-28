cb8a30efdced1392bc410825c38e45455a55ccbb
zrajani <zainrajani2207@gmail.com>
2022-08-28 16:29:33 -0400

    Intensity functions in MATLAB

--- MATLAB/Image Intensity Transformation/Intensity_Transformation.m
+++ MATLAB/Image Intensity Transformation/Intensity_Transformation.m
@@ -2,6 +2,7 @@
 % Author: Zain Rajani
 
 % Program Name: Image Intensity Transformation (Monochrome Image)
+% Gamma Correction (a.k.a)
 
 %% Initialize
 clear all;
@@ -21,21 +22,31 @@ f_comp=imcomplement(f);
 %% Intensity Transformation functions
 
 g1=imadjust(f,[0 1],[1 0]); % Getting Negative Image Alternative suggestion (Negative Image)
-g2=imadjust(f,[0.5 0.75],[1 0],2);
+g2=imadjust(f,[0.5 0.75],[0.25 1],2);
 
 %% Display Images 
 
 % Diffrent Image operations are shown using the subplot command 
 figure;
-subplot (2,2,1)
+subplot (3,2,1)
 imshow(f);
 title('Input Image')
-subplot (2,2,2)
-imshow(g1)
-title('Negative Image')
-subplot(2,2,3)
+subplot (3,2,2)
 imshow(f_comp)
 title('Negative Image')
-subplot(2,2,4)
+subplot(3,2,3)
+imshow(g1)
+title('Negative Image')
+subplot(3,2,4)
 imshow(g2)
-title('Adjusting Intensities with parameters')
+title('Adjusting Intensities with \gamma=2')
+
+g3=imadjust(f,[],[],0.25);
+subplot(3,2,5)
+imshow(g3);
+title('Adjusting Intensities with \gamma=0.25')
+
+g=imadjust(f,stretchlim(f),[ ]);
+subplot(3,2,6)
+imshow(g)
+title('Image Intensities with stretchlim() \gamma=0')
